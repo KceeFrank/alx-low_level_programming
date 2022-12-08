e20d1f0fbf2c1f6c7d57481a568a93560eb1983d
KceeFrank <fimboubolt@gmail.com>
2022-12-08 13:06:08 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main -using put to print a line of code 
+/**
+ * main -using put to print a line of code
  *
- * return:0
- */
+ * return: 0
+*/
+
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
