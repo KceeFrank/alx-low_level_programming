cd59407e4cd5d5a087af5d3377fc52f5798c27c1
KceeFrank <fimboubolt@gmail.com>
2022-12-08 13:47:50 -0800

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main -using put to print a line of code
+ * main -Print a line of code using puts
  *
- * return: 0
+ * Return: 0
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
