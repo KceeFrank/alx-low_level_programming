66c6e1c6d96c7b861c1e42ee0beb6f33b24e7d9d
KceeFrank <fimboubolt@gmail.com>
2022-12-23 13:07:15 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
-
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index)
-		dest[index] = '\0';
-
+	strcpy(dest, src, n);
 	return (dest);
 }
