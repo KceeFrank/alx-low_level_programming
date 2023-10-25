3dcb7cb651fb3987b49eb99940ff3971da27a775
“kceefrank” <fimboubolt@gmail.com>
2023-10-25 07:09:52 -0700

    dreams feel real

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,22 +1,19 @@
-/*
- * File: 2-strlen_recursion.c
- * author: kaziro
- */
-
 #include "main.h"
-
 /**
- * _strlen_recursion - Return the lemgth of a string.
+ * _strlen_recursion - Returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-	s = "lpa\0;
+	int longit = 0;
 
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (1 + _strlen_recursion(s));
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+
+	return (longit);
 }
